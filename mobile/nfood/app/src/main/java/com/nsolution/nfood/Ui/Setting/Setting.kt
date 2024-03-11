4baf4fc433aacec336fec57295262329dbebf597
unknown <nguyenquochuydl123@gmail.com>
2024-03-11 10:21:07 +0700

    Add mobile -> user

---
+++ mobile/nfood/app/src/main/java/com/nsolution/nfood/Ui/Setting/Setting.kt
@@ -0,0 +1,30 @@
+package com.nsolution.nfood.Ui.Setting
+
+import android.os.Bundle
+import com.nsolution.nfood.R
+import com.nsolution.nfood.Ui.Base.BaseActivity
+
+class Setting : BaseActivity() {
+  
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_setting)
+    initialView()
+  }
+  
+  private fun initialView() {
+//    getBackActionBar(header, getString(R.string.setting))
+//
+//    updateProfileButton.setOnClickListener {
+//      navigateTo(UpdateProfile::class.java)
+//    }
+//
+//    notificationSettingButton.setOnClickListener {
+//      navigateTo(NotificationSetting::class.java)
+//    }
+//
+//    languageSettingButton.setOnClickListener {
+//      navigateTo(LanguageSetting::class.java)
+//    }
+  }
+}
